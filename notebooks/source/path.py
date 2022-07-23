996f9cbb93dd82eb448daef731fb6512dd09be04
hussein <eslam@idia.ac.za>
2022-07-23 19:30:53 +0200

    done

--- notebooks/source/path.py
+++ notebooks/source/path.py
@@ -2,11 +2,11 @@ import os
 from pathlib import Path
 
 def image_path(freq):
-    return os.path.join("data", "sample_images", "{}mhz_1000h.fits".format(freq))
+    return os.path.join("../data", "sample_images", "{}mhz_1000h.fits".format(freq))
 
 
 def pb_path(freq):
-    return os.path.join("data", "sample_images", "{}mhz_pb.fits".format(freq))
+    return os.path.join("../data", "sample_images", "{}mhz_pb.fits".format(freq))
 
 
 def train_truth_path(freq):
@@ -20,23 +20,23 @@ def full_truth_path(freq):
 # Output data paths
 #
 def train_source_df_path(freq):
-    return os.path.join("data", "sources", "{}mhz_sources_train.csv".format(freq))
+    return os.path.join("../data", "sources", "{}mhz_sources_train.csv".format(freq))
 
 
 def full_source_df_path(freq):
-    return os.path.join("data", "sources", "{}mhz_sources_full.csv".format(freq))
+    return os.path.join("../data", "sources", "{}mhz_sources_full.csv".format(freq))
 
 
 def submission_df_path(freq):
-    return os.path.join("data", "sources", "{}mhz_submission.csv".format(freq))
+    return os.path.join("../data", "sources", "{}mhz_submission.csv".format(freq))
 
 
 def model_path(freq):
-    return os.path.join("data", "models", "{}mhz_classifier.pickle".format(freq))
+    return os.path.join("../data", "models", "{}mhz_classifier.pickle".format(freq))
 
 
 def score_report_path(freq):
-    return os.path.join("data", "score", "{}mhz_score.txt".format(freq))
+    return os.path.join("../data", "score", "{}mhz_score.txt".format(freq))
 
 
 def write_df_to_disk(df, out_path):
